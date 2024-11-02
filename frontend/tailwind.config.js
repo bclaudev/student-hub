4d6cc3c47a8ac00d8a48ebff4c31439273e854e3
bclaudev <claudia_burcea@outlook.com>
2024-11-02 23:37:08 +0200

    Added back styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './*.html',        // Adjust this path if your HTML files are in a different directory
+    './public/index.html',        // Adjust this path if your HTML files are in a different directory
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        customPurple: '#A585FF',
+        customPurple: '#A585FF'
       },
     },
   },
